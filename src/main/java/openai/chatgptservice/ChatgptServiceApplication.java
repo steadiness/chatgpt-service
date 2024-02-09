47f2c632ed408eb2af1134820f14c2a2f82e19f8
DESKTOP-LO8UF64\LDCC <hyeonjun.sin@lotte.net>
2024-02-09 15:05:53 +0900

    Initial commit

---
+++ src/main/java/openai/chatgptservice/ChatgptServiceApplication.java
@@ -0,0 +1,13 @@
+package openai.chatgptservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgptServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/openai/chatgptservice/ChatgptServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package openai.chatgptservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void apiTest(){
+		
+	}
+
+}
